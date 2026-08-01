198547d8baeb711e96eec55f43761a0e41c48ef4
jm-733 <jamaicamaeugbinada733@gmail.com>
2026-08-01 14:34:28 +0800

    Refactor unlockLetter function for consistency

--- script.js
+++ script.js
@@ -1,18 +1,32 @@
 function unlockLetter(){
 
-    const password = document.getElementById("password").value;
+    const password=document.getElementById("password").value;
 
-    if(password === "jamsen28"){
+    if(password==="jamsen28"){
 
-        document.getElementById("lockScreen").style.display = "none";
+        document.getElementById("lockScreen").style.display="none";
 
-        document.getElementById("galleryScreen").style.display = "flex";
+        document.getElementById("galleryScreen").style.display="flex";
+
+        document.getElementById("bgMusic").play();
+
+        const photos=document.querySelectorAll(".photo");
+
+        photos.forEach((photo,index)=>{
+
+            setTimeout(()=>{
+
+                photo.classList.add("show");
+
+            },index*1800);
+
+        });
 
     }
 
     else{
 
-        document.getElementById("error").innerHTML = "Wrong password 💔";
+        document.getElementById("error").innerHTML="Wrong password 💔";
 
     }
 
